a16b4626641d6768b0038a593f0ca22b4ac0e3c7
Jonny Sanchez <hec-tro20112011@gmail.com>
2018-09-20 21:12:34 -0500

    primer commit0

---
+++ app/src/main/java/s/pruema/com/prueba/MainActivity.kt
@@ -0,0 +1,12 @@
+package s.pruema.com.prueba
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
